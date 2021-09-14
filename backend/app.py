0f18aef2065a272b215eaa057f2b6162bd44a65d
SimaDovakin <simadovakin999@gmail.com>
2021-09-15 00:16:07 +0300

    Create Book model

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,25 @@
+import datetime
+
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    author = db.Column(db.String(80), nullable=False, unique=True)
+    creted_at = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __init__(self, name, author):
+        self.name = name
+        self.author = author
 
 @app.route('/', methods=['GET'])
 def ping():
